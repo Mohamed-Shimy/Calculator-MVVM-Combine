cce7a5b9e0f36f2171de0dff92f164b5bda65c1e
Mohamed Shemy <msh.work@outlook.sa>
2022-01-23 06:37:31 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mohamed Shemy on Sun 23 Jan, 2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
